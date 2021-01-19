b641564be8f5a2c39c91b1d0ac59b306810983f2
K S PAVAN KRISHNA <58327514+kspavankrishna@users.noreply.github.com>
2021-01-20 00:24:06 +0530

    Delete Part A directory

--- Part A/4 FP Tree.r
+++
@@ -1,9 +0,0 @@
-library("rJava")
-library("rCBA")
-data("iris")
-
-train <- sapply(iris,as.factor)
-train <- data.frame(train,check.names = FALSE) 
-txns <- as(train,"transactions")
-rules = rCBA::fpgrowth(txns,support=0.03,confidence = 0.03,maxLength = 1,consequent = "Species",parallel = FALSE)
-inspect(rules)
